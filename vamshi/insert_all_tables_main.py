fb1908f41086a3739753e1854c81b796ae891116
vamshiteja <rachavamshiteja@gmail.com>
2018-02-21 18:28:05 +0530

    populate

--- vamshi/insert_all_tables_main.py
+++ vamshi/insert_all_tables_main.py
@@ -2,7 +2,7 @@
 # @Author: vamshi
 # @Date:   2018-02-19 23:34:40
 # @Last Modified by:   vamshi
-# @Last Modified time: 2018-02-21 10:29:58
+# @Last Modified time: 2018-02-21 18:25:41
 
 import sys
 import os
@@ -22,26 +22,87 @@ name_file = data_dir + "name.basics.tsv"
 def create_tables():
     """ create tables in the PostgreSQL database"""
     commands = (
+
         """
-        CREATE TABLE movie_people (
-            PersonID VARCHAR(255) PRIMARY KEY,
-            Name VARCHAR(255),
-            birthYear VARCHAR(255),
-            deathYear VARCHAR(255),
-            primaryProfession VARCHAR(255),
-            knownForTitles VARCHAR(255)
+        CREATE TABLE MOVIE (
+            movieID INTEGER PRIMARY KEY,
+            primary_title VARCHAR(255),
+            original_title VARCHAR(255),
+            isAdult boolean,
+            release_year VARCHAR(255),
+            run_time VARCHAR(255),
+            avg_rating DECIMAL,
+            num_votes DECIMAL
+            )
+        """,
+        """
+        CREATE TABLE GENRE (
+            genreID SERIAL PRIMARY KEY,
+            genre VARCHAR(225)
+        )
+        """,
+
+        """
+        CREATE TABLE TV_SHOW(
+        	showID INTEGER PRIMARY KEY,
+            primary_title VARCHAR(255),
+            original_title VARCHAR(255),
+            isAdult boolean,
+            start_year VARCHAR(255),
+            end_year VARCHAR(255),
+            run_time VARCHAR(255),
+            avg_rating DECIMAL,
+            num_votes DECIMAL
         )
         """,
         """
-        CREATE TABLE movie (
-            MovieID INTEGER PRIMARY KEY,
-            release_date VARCHAR(255),
-            run_time INTEGER,
-            title VARCHAR(255)
+        CREATE TABLE TV_episode(
+        	epsoID INTEGER PRIMARY KEY,
+            primary_title VARCHAR(255),
+            original_title VARCHAR(255),
+            release_year VARCHAR(255),
+            run_time VARCHAR(255),
+            showID INTEGER REFERENCES TV_SHOW,
+            season_No VARCHAR(255),
+            epso_No VARCHAR(255),
+            avg_rating DECIMAL,
+            num_votes DECIMAL
+        	)
+        """,
+        """
+        CREATE TABLE DIRECTOR(
+        	dirID INTEGER PRIMARY KEY
         )
-        """)
+        """,
+        """
+        CREATE TABLE WRITER(
+        	wriID INTEGER PRIMARY KEY
+        )
+        """,
+        """
+        CREATE TABLE PRINCIPALS(
+        	perID INTEGER PRIMARY KEY,
+        	job_category VARCHAR(255)  
+        )
+        """,
 
-    copy_commands = ("""COPY movie_people FROM '/home/vamshi/BTECH/SEM 6/DBMS II/Project/Data/name.basics.tsv' DELIMITER '\t' """)
+        """
+        CREATE TABLE MOVIE_PEOPLE (
+            PersonID VARCHAR(255) PRIMARY KEY,
+            Name VARCHAR(255),
+            birthYear VARCHAR(255),
+            deathYear VARCHAR(255)
+        )
+        """
+
+          
+        )
+
+    copy_commands = ("""COPY MOVIE FROM '/home/vamshi/BTECH/SEM 6/DBMS II/Project/asg2/movies.csv' DELIMITER '\t' CSV HEADER""",
+    				"""COPY TV_SHOW FROM '/home/vamshi/BTECH/SEM 6/DBMS II/Project/asg2/tv_shows.csv' DELIMITER '\t' CSV HEADER""",
+    				"""COPY TV_episode FROM '/home/vamshi/BTECH/SEM 6/DBMS II/Project/asg2/tv_eps.csv' DELIMITER '\t' CSV HEADER""",
+    				"""COPY MOVIE_PEOPLE FROM '/home/vamshi/BTECH/SEM 6/DBMS II/Project/asg2/movie_people.csv' DELIMITER '\t' CSV HEADER""",
+    				)
 
     c = ("""CREATE TEMP TABLE  temp_table (id bigint, dob bigint )""",
     	"""COPY temp_table '/home/vamshi/BTECH/SEM 6/DBMS II/Project/Data/name.basics.tsv' delimiter '\t'  csv header""",
@@ -58,8 +119,8 @@ def create_tables():
         # create table one by one
         for command in commands:
         	cur.execute(command)
-        
-        cur.execute(copy_commands)
+        for cmd in copy_commands:
+        	cur.execute(cmd)
         # close communication with the PostgreSQL database server
         cur.close()
         # commit the changes
